7fba2ac0e5671a4d9de07b8ba5ebcb7a62897978
GouravIN <gourav.pattnaik@internations.org>
2020-12-04 12:02:32 +0100

    Loading shared lib implicitly

--- vars/printMessage.groovy
+++ vars/printMessage.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def printNode(name) {
+def call(name) {
     echo "I'm running on node ${env.NODE_NAME}"
     sh(script: "sleep 60", label: "Hello ${name},I'm sleeping for a minute now")
 }
