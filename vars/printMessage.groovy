d1ea42cbafb6ed67bef932f02cfa7d8790f71c6a
GouravIN <gourav.pattnaik@internations.org>
2021-02-02 11:40:25 +0100

    Using class

--- vars/printMessage.groovy
+++ vars/printMessage.groovy
@@ -1,7 +1,8 @@
 #!/usr/bin/env groovy
-import tools
-def t=new tools()
+GroovyShell shell = new GroovyShell()
+def tools = shell.parse(new File('tools_007.groovy'))
+
 def call(name) {
     echo "I'm running on node ${env.NODE_NAME} and my name is ${name}"
-    t.hello()
+    tools.hello()
 }
