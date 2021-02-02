76f87b60a558c67fec33f9f57c8feccfcc0d9f66
GouravIN <gourav.pattnaik@internations.org>
2021-02-02 17:07:51 +0100

    testing import from call method

--- vars/command.groovy
+++ vars/command.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
-def call(Map args = [:], String script) {
-    script = "${args.sudo ? 'sudo ' : ''}${script}"
-    def label = args.label ?: script
+def call(Map args = [:]) {
+    def script = args.script
+    def label = args.label
     sh label: label, script: script
 }
