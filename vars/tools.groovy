06a6a2cc78c3ebe1950d5cf327c4dfe2d42ba827
GouravIN <gourav.pattnaik@internations.org>
2021-02-02 12:15:36 +0100

    Using class

--- vars/tools.groovy
+++ vars/tools.groovy
@@ -1,8 +1,6 @@
 class tools {
-    static def hello() {
-        def sh = {sh "sleep 10"}
-        sh.call()
-        def echo = {echo "I RAN"}
-        echo.call()
+    static def hello(String args) {
+        sh "sleep 10"
+        echo "I RAN"
     }
 }
