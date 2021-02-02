b3ac5ace9b3f14d2a4ef3e3fd5b6bf2291b12593
GouravIN <gourav.pattnaik@internations.org>
2021-02-02 12:02:12 +0100

    Using class

--- vars/printMessage.groovy
+++ vars/printMessage.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/env groovy
-def tools = new GroovyScriptEngine( '.' ).with {
-    loadScriptByName( 'tools.groovy' )
-}
-this.metaClass.mixin tools
-
+import tools
+def t = new tools()
 def call(name) {
     echo "I'm running on node ${env.NODE_NAME} and my name is ${name}"
-    hello()
+    new tools().hello()
 }
